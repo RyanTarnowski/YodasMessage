4adf9ed8ce915dd2ec93771376a2ad0c0eddbb24
RyanTarnowski <ryan.tarnowski@gmail.com>
2025-01-02 16:35:06 -0700

    Program randomized message.

--- YodasMessage.js
+++ YodasMessage.js
@@ -1,33 +1,34 @@
 const greeting = [
-    "Greetings,", 
-    "Welcome,", 
-    "Happy to see you I am,", 
-    ", a message for you I have."
+    "Greetings", 
+    "Welcome", 
+    "Happy to see you I am", 
+    "A message for you I have"
 ];
 
 const person = [
     "Padawan",
     "Apprentice",
-    "Master",
+    "Master Jedi",
     "Dark One",
 ];
 
 const quote = [
-    "Do or do not, there is no try.",
-    "Unlearn what you have learned.",
-    "Pass on what you have learned.",
-    "The fear of loss is a path to the dark side.",
-    "The greatest teacher, failure is.",
-    "Named must your fear be, before banish it you can.",
-    "Your path you must decide.",
-    "Fear is the path to the dark side.",
-    "Fear leads to anger.",
-    "Anger leads to hate.",
-    "Hate leads to suffering.",
-    "Remember, a Jedi's strength flows from the Force.",
+    "Do or do not, there is no try",
+    "Unlearn what you have learned",
+    "Pass on what you have learned",
+    "The fear of loss is a path to the dark side",
+    "The greatest teacher, failure is",
+    "Named must your fear be, before banish it you can",
+    "Your path you must decide",
+    "Fear is the path to the dark side",
+    "Fear leads to anger",
+    "Anger leads to hate",
+    "Hate leads to suffering",
+    "Remember, a Jedi's strength flows from the Force",
 ];
 
+const randGreeting = greeting[Math.floor(Math.random() * greeting.length)];
+const randPerson = person[Math.floor(Math.random() * person.length)];
+const randQuote = quote[Math.floor(Math.random() * quote.length)];
 
-console.log(greeting);
-console.log(person);
-console.log(quote);
+console.log(`${randGreeting}, ${randPerson}. ${randQuote}.`);
